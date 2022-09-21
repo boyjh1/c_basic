cf2eafa60d114940216966e6da2951e507e780f1
boyjh1 <112962799+boyjh1@users.noreply.github.com>
2022-09-21 10:47:10 +0900

    finished chapter 2

---
+++ chapter2/c_print_char.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * vs code 단축키
+ * ctrl + alt + n: c 실행
+ * ctrl+ alt + ↓ or ↑: line 복사
+ * alt + ↓ + ↑: line 이동
+ * ctrl + x: 한줄 삭제
+ * ctrl + space: 자동 완성
+ * '': 문자(character) 'A' 'B' + 키보드 제어를 할 때 사용
+ * "": 문자열(string) "A", "ABC" "A!"
+*/
+
+
+int main(void)
+{
+    /* code*/
+    printf('%c\n', 'A');
+    printf("%c은 %s입니다.\n", '1', "first");
+    return 0;
+}
