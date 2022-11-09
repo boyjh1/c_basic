794da091c0fe7489e53f9c1cd7e9efaa110a08b8
boyjh1 <112962799+boyjh1@users.noreply.github.com>
2022-11-09 16:10:47 +0900

    Add files via upload

---
+++ c_workspace/chapter7/c_method_parameters.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * int main() {
+ *    printf("%d", sum(5,10));
+ * }
+ * 
+ *    ก้          (return)
+ *    ก้             ก่
+ *    ก้             ก่ 
+ *(parameters)      ก่
+ * int sum(int x, int y) {
+ *    return x + y;
+ * }
+ * 
+ * 
+*/
+int get_num(void);
+
+int main (void) {
+
+}
+
+int get_num(void) {
+    int num;
+    printf("number: ")
+    scanf("%d", &num)
+    return num;
+}
--- c_workspace/project/pj1.c
+++ c_workspace/project/pj1.c
@@ -18,3 +18,19 @@ int main(void){
 // 5 + 7 + 2 + 5 + 6 = 25
     printf("총합 %d\n", total);
 }
+
+
+int main(void) {
+    int num = 57256;
+    int total = 0;
+
+    for(int i=1; i<=5; i++) {
+        num = num / 10;
+        total += (num % 10);
+    }
+
+    printf("총합 %d\n", total);
+}
+
+// 과제 1.숫자를 사용자에게 입력(scanf)
+//자리 수 없는 상황에서도 똑같이 동작
